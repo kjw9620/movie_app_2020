6a2c5e81b8f01f768cadb1c5fc57fe6072559617
JongWon Kim <kjw3829620@naver.com>
2020-07-24 17:15:23 +0900

    component life cycle

--- src/App.js
+++ src/App.js
@@ -1,83 +1,47 @@
 import React from 'react';
 import propTypes from "prop-types";
 
-const foodILike = [
-  {
-    id:1,
-    name: "Kimchi",
-    rating: 1.0,
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
-  },
-  {
-    id:2,
-    name: "Samgyeopsal",
-    rating: 2.1,
-    image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
-  },
-  {
-    id:3,
-    name: "Bibimbap",
-    rating: 3.2,
-    image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
-  },
-  {
-    id:4,
-    name: "Doncasu",
-    rating: 4.5,
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
-  },
-  {
-    id:5,
-    name: "Kimbap",
-    rating: 5.0,
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
-  }
-];
-
-
-function Food( {name, picture, rating} ) {
-  console.log(name);
-  return <div>
-    <h1> I love {name} </h1>
-    <h4> {rating}/5.0 </h4>
-    <img src={picture} alt={name}/>
-  </div>
+class App extends React.Component{
   
-};
+  constructor() {
+    console.log("constructor");
+  }
 
-function renderFood(dish) {
+  state = {
+    count: 0
+  };
 
-  return (
-    <Food 
-    
-    key={dish.id}
-    name={dish.name}
-    picture={dish.image}
-    rating={dish.rating}
+  add = () => {
+    console.log("add");
+    this.setState(current => ({count: current.count+1}));
+  };
 
-    />
-  )
-};
+  minus = () => {
+    console.log("minus");
+    this.setState(current => ({count: current.count-1}));
+  };
+  
+  componentDidMount() {
+    console.log("component rendered");
+  }
 
-Food.propTypes = {
-  name: propTypes.string.isRequired,
-  picture: propTypes.string.isRequired,
-  rating: propTypes.number.isRequired
-};
+  componentDidUpdate() {
+    console.log("component updated");
+  }
 
-function App() {
-  return (
-    <div>
-      <h1>hello</h1>
-      {foodILike.map(renderFood)}
-      
-    </div>
-  );
+  render(){
+    console.log("Im rendering");
+
+    return (
+      <div>
+        <h1>This number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button> 
+      </div>
+    )
+  }
+
+
 }
 
 export default App;
